d5d3394912e2a99d55b7e3db4d35e5dd2dd86834
Russell Aronson <russell.aronson@gmail.com>
2013-11-17 16:42:05 +1100

    Added marks md5 checksum impl, tarball/gzip validation functions, more file read/write helpers, more tests

--- src/main/scala/com/ambiata/mundane/io/Files.scala
+++ src/main/scala/com/ambiata/mundane/io/Files.scala
@@ -1,20 +1,99 @@
 package com.ambiata.mundane
 package io
 
-import java.io.File
+import java.io._
 import java.security.MessageDigest
 import scalaz._, Scalaz._
 
 object Files {
   def calcChecksum(f: File): String \/ String =
-    readFileBytes(f).map(md5Hex)
+    checksumFile(f).map(toHexString)
 
-  def md5Hex(bytes: Array[Byte]): String =
-    md5Bytes(bytes).map("%02X".format(_)).mkString.toLowerCase
+  def toHexString(bytes: Array[Byte]): String =
+    bytes.map("%02X".format(_)).mkString.toLowerCase
 
-  def md5Bytes(bytes: Array[Byte]): Array[Byte] =
-    MessageDigest.getInstance("MD5").digest(bytes)
+  def unsafeChecksum(is: InputStream): Array[Byte] = {
+    val buffer = Array.ofDim[Byte](4096)
+    var length = 0
+    val digester = MessageDigest.getInstance("MD5")
+    while ({ length = is.read(buffer, 0, buffer.length); length != -1 }) {
+      digester.update(buffer, 0, length)
+    }
+    digester.digest
+  }
+
+  def checksumFile(f: File): String \/ Array[Byte] =  {
+    val fis = new FileInputStream(f)
+    val res = checksumStream(fis)
+    fis.close()
+    res
+  }
+
+  def checksumStream(is: InputStream): String \/ Array[Byte] = 
+    try unsafeChecksum(is).right catch { case t: Throwable => t.getMessage.left }
+
+  def readFile(f: File): String \/ String = {
+    try {
+      val source = scala.io.Source.fromFile(f)
+      val lines = source.getLines.mkString
+      source.close()
+      lines.right
+    } catch { case e: Exception => e.getMessage.left }
+  }
 
   def readFileBytes(f: File): String \/ Array[Byte] =
     try org.apache.commons.io.FileUtils.readFileToByteArray(f).right catch { case t: Throwable => t.getMessage.left }
+
+  def writeInputStream(is: InputStream, os: OutputStream): String \/ OutputStream =
+    try { org.apache.commons.io.IOUtils.copy(is, os); os.right } catch { case t: Throwable => t.getMessage.left }
+
+  def writeInputStreamToFile(is: InputStream, f: File): String \/ File = {
+    val os = new FileOutputStream(f)
+    val res = writeInputStream(is, os)
+    os.close()
+    res.map(_ => f)
+  }
+
+  def printToFile(f: File)(op: java.io.PrintWriter => Unit): String \/ File = {
+    val p = new java.io.PrintWriter(f)
+    try { op(p) }
+    catch { case e: Exception => "Could not write to file '${f.getPath}' - '${e.getMessage}".left }
+    finally { p.close() }
+    f.right
+  }
+
+  def mv(src: File, dest: File): String \/ File =
+    try
+      if(src.renameTo(dest)) dest.right else s"Could not move ${src.getPath} to ${dest.getPath}".left
+    catch {
+      case t: Throwable => t.getMessage.left
+    }
+
+  def validGzip(f: File): Boolean =
+    !gzipError(f).isDefined
+
+  def gzipError(f: File): Option[String] = {
+    import scala.sys.process._
+    val sw = new StringWriter
+    if(f.length == 0)
+      Some(s"${f.getAbsolutePath} is empty")
+    else if((List("sh", "-c", s"gzip -dc ${f.getPath} > /dev/null") ! ProcessLogger(o => (), e => sw.write(s"${e}\n"))) != 0)
+      Some(sw.toString)
+    else
+      None
+  }
+
+  def validTarball(f: File): Boolean =
+    !tarballError(f).isDefined
+
+  def tarballError(f: File): Option[String] = {
+    import scala.sys.process._
+    val sw = new StringWriter
+    if(f.length == 0)
+      Some(s"${f.getAbsolutePath} is empty")
+    else if((List("sh", "-c", s"tar xfz ${f.getPath} -O > /dev/null") ! ProcessLogger(o => (), e => sw.write(s"${e}\n"))) != 0)
+      Some(sw.toString)
+    else
+      None
+  }
 }
--- src/test/scala/com/ambiata/mundane/io/FilesSpec.scala
+++ src/test/scala/com/ambiata/mundane/io/FilesSpec.scala
@@ -8,30 +8,100 @@ import java.security.MessageDigest
 class FilesSpec extends Specification with ScalaCheck { def is = s2"""
 
 Files should be able to:
-  read bytes from file                        $read
-  calculate checksums of a file               $calcChecksum
-
+  read bytes from file                        $e1
+  read string from file                       $e2
+  calculate checksum of a file                $e3
+  validate tarball                            $e4
+  validate gzip                               $e5
 """
 
-  def read = prop((bs: Array[Byte]) => {
+  def e1 = prop((bs: Array[Byte]) => {
     val tmpFile = File.createTempFile("files-spec", ".bytes")
-    val fos = new FileOutputStream(tmpFile)
-    fos.write(bs)
+    writeBytes(tmpFile, bs)
     val fileBytes = Files.readFileBytes(tmpFile)
-    tmpFile.delete
+    tmpFile.delete()
     fileBytes.toOption.get === bs
   })
 
-  def calcChecksum = prop((bs: Array[Byte]) => {
-    val tmpFile = File.createTempFile("files-calc-checksum-spec", ".bytes")
-    val fos = new FileOutputStream(tmpFile)
-    fos.write(bs)
-    val checksum = Files.calcChecksum(tmpFile)
-    tmpFile.delete
-
-    val expectedChecksum = MessageDigest.getInstance("MD5").digest(bs).map("%02X".format(_)).mkString.toLowerCase
-
-    checksum.toOption === Some(expectedChecksum)
-
+  def e2 = prop((str: String) => {
+    val tmpFile = File.createTempFile("files-spec", ".string")
+    writeString(tmpFile, str)
+    val fileStr = Files.readFile(tmpFile)
+    tmpFile.delete()
+    fileStr.toOption.get === str
   })
+
+  def e3 = prop((bs: Array[Byte]) => {
+    val tmpFile = File.createTempFile("files-calc-checksum-spec", ".bytes")
+    writeBytes(tmpFile, bs)
+    val checksum = Files.calcChecksum(tmpFile)
+    tmpFile.delete()
+    val expectedChecksum = MessageDigest.getInstance("MD5").digest(bs).map("%02X".format(_)).mkString.toLowerCase
+    checksum.toOption === Some(expectedChecksum)
+  })
+
+  def e4 = prop((bs: Array[Byte], str: String) => {
+    val tmpDir = mkTempDir("files-spec")
+    val tmpBinFile = File.createTempFile("files-spec", ".bytes", tmpDir)
+    val tmpStrFile = File.createTempFile("files-spec", ".string", tmpDir)
+    val tmpTgzFile = File.createTempFile("files-spec", ".tar.gz")
+
+    writeBytes(tmpBinFile, bs)
+    writeString(tmpStrFile, str)
+    tarball(tmpDir, tmpTgzFile)
+
+    Files.validTarball(tmpTgzFile) === true
+    Files.tarballError(tmpStrFile) must beSome
+
+    rmdir(tmpDir)
+    tmpTgzFile.delete()
+  })
+
+  def e5 = prop((str: String) => {
+    val tmpStrFile = File.createTempFile("files-spec", ".string")
+
+    writeString(tmpStrFile, str)
+    val tmpGzipFile = gzip(tmpStrFile)
+
+    Files.validGzip(tmpGzipFile) === true
+    Files.validGzip(tmpStrFile) === false
+  })
+
+
+  def mkTempDir(prefix: String, suffix: String = System.nanoTime.toString): File = {
+    val tmpFile = File.createTempFile(prefix, suffix)
+    if(!tmpFile.delete()) sys.error(s"Could not delete temp file '${tmpFile.getAbsolutePath}'")
+    if(!tmpFile.mkdir()) sys.error(s"Could not create temp dir '${tmpFile.getAbsolutePath}'")
+    tmpFile
+  }
+
+  def tarball(inDir: File, outFile: File): File = {
+    import scala.sys.process._
+    Process(List("sh", "-c", s"tar czf ${outFile.getPath} --directory ${inDir.getPath} .")).run(ProcessLogger(stdout => (), println))
+    outFile
+  }
+
+  def gzip(file: File): File = {
+    import scala.sys.process._
+    val gzfile = new File(file.getPath + ".gz")
+    Process(List("sh", "-c", s"gzip -c ${file.getPath} > ${gzfile.getPath}")).run(ProcessLogger(o => (), println))
+    gzfile
+  }
+
+  def writeBytes(f: File, bytes: Array[Byte]) {
+    val fos =  new FileOutputStream(f)
+    fos.write(bytes)
+    fos.close()
+  }
+
+  def writeString(f: File, str: String) {
+    val pw = new PrintWriter(f)
+    pw.write(str)
+    pw.close()
+  }
+
+  def rmdir(d: File) {
+    if(d.isDirectory) d.listFiles.foreach(rmdir) else d.delete
+    d.delete
+  }
 }
