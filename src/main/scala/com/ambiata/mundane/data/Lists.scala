99ea7982dfd261ba6f8b64d0442d34fb1756be7b
markhibberd <mark@hibberd.id.au>
2014-01-24 09:56:53 +1100

    Fix lazy pattern matching in Lists.

--- src/main/scala/com/ambiata/mundane/data/Lists.scala
+++ src/main/scala/com/ambiata/mundane/data/Lists.scala
@@ -3,7 +3,7 @@ package data
 
 object Lists {
   def prepareForFile(xs: List[String]) = xs match {
-    case Nil => ""
-    case xs  => xs.mkString("\n") + "\n"
+    case Nil       => ""
+    case _ :: Nil  => xs.mkString("\n") + "\n"
   }
 }
