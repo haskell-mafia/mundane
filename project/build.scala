9f080c414b6d0fc97b122e3d9add5a67894ea645
etorreborre <etorreborre@yahoo.com>
2014-02-24 10:12:06 +1100

    using sbt 0.13.2-M2

--- project/build.scala
+++ project/build.scala
@@ -26,6 +26,7 @@ object build extends Build {
   lazy val compilationSettings: Seq[Settings] = Seq(
     javacOptions ++= Seq("-Xmx3G", "-Xms512m", "-Xss4m"),
     maxErrors := 20,
+    incOptions := incOptions.value.withNameHashing(true),
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-language:_", "-Ywarn-value-discard"),
     scalacOptions in Test ++= Seq("-Yrangepos")
   )
