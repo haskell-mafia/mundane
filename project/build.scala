8a84004a857cbc5851bdf7f0336a00a370824341
etorreborre <etorreborre@yahoo.com>
2014-03-14 12:11:22 +1100

    first cut at modularising mundane

--- project/build.scala
+++ project/build.scala
@@ -6,21 +6,101 @@ object build extends Build {
   type Settings = Def.Setting[_]
 
   lazy val mundane = Project(
-    id = "mundane",
-    base = file("."),
-    settings = Defaults.defaultSettings ++
-               projectSettings          ++
-               compilationSettings      ++
-               testingSettings          ++
-               publishingSettings       ++
-               packageSettings
+      id = "mundane"
+    , base = file(".")
+    , settings = standardSettings          
+    , aggregate = Seq(cli, control, data, error, io, parse, reflect, testing, time)
     )
 
+  lazy val standardSettings = Defaults.defaultSettings ++
+                   projectSettings          ++
+                   compilationSettings      ++
+                   testingSettings          ++
+                   publishingSettings       ++
+                   packageSettings         
+
   lazy val projectSettings: Seq[Settings] = Seq(
-    name := "mundane",
-    version in ThisBuild := "1.2.1",
-    organization := "com.ambiata",
-    scalaVersion := "2.10.3"
+      name := "mundane"
+    , version in ThisBuild := "1.2.1"
+    , organization := "com.ambiata"
+    , scalaVersion := "2.10.3"
+  ) ++ Seq(prompt)
+
+  lazy val cli = Project(
+    id = "cli"
+  , base = file("mundane-cli")
+  , settings = standardSettings ++ Seq[Settings](
+      name := "mundane-cli"
+    ) ++ Seq[Settings](libraryDependencies ++= depend.scopt ++ depend.scalaz ++ depend.joda)
+  )
+
+  lazy val control = Project(
+    id = "control"
+  , base = file("mundane-control")
+  , settings = standardSettings ++ Seq[Settings](
+      name := "mundane-control"
+    ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.specs2)
+  )
+  .dependsOn(error)
+
+  lazy val data = Project(
+    id = "data"
+  , base = file("mundane-data")
+  , settings = standardSettings ++ Seq[Settings](
+      name := "mundane-data"
+    ) ++ Seq[Settings](libraryDependencies ++= depend.rng ++ depend.specs2 ++ depend.kiama)
+  )
+
+  lazy val error = Project(
+    id = "error"
+  , base = file("mundane-error")
+  , settings = standardSettings ++ Seq[Settings](
+      name := "mundane-error"
+    )
+  )
+
+  lazy val io = Project(
+    id = "io"
+  , base = file("mundane-io")
+  , settings = standardSettings ++ Seq[Settings](
+      name := "mundane-io"
+    ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.joda ++ depend.commonsio ++ depend.specs2 ++ depend.scrutiny)
+  )
+  .dependsOn(control)
+  .dependsOn(data)
+
+  lazy val parse = Project(
+    id = "parse"
+  , base = file("mundane-parse")
+  , settings = standardSettings ++ Seq[Settings](
+      name := "mundane-parse"
+    ) ++ Seq[Settings](libraryDependencies ++= depend.parboiled ++ depend.joda)
+  )
+  .dependsOn(control)
+
+  lazy val reflect = Project(
+    id = "reflect"
+  , base = file("mundane-reflect")
+  , settings = standardSettings ++ Seq[Settings](
+      name := "mundane-reflect"
+    )
+  )
+
+  lazy val testing = Project(
+    id = "testing"
+  , base = file("mundane-testing")
+  , settings = standardSettings ++ Seq[Settings](
+      name := "mundane-testing"
+    ) ++ Seq[Settings](libraryDependencies ++= depend.specs2)
+  )
+  .dependsOn(control)
+
+  lazy val time = Project(
+    id = "time"
+  , base = file("mundane-time")
+  , settings = standardSettings ++ Seq[Settings](
+      name := "mundane-time"
+    ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.joda ++ depend.specs2)
   )
 
   lazy val compilationSettings: Seq[Settings] = Seq(
@@ -55,4 +135,10 @@ object build extends Build {
     },
     credentials += Credentials(Path.userHome / ".credentials")
   )
+
+  lazy val prompt = shellPrompt in ThisBuild := { state => 
+    val name = Project.extract(state).currentRef.project
+    (if (name == "mundane") "" else name) + "> " 
+  }
+
 }
---
+++ project/depend.scala
@@ -0,0 +1,28 @@
+import sbt._
+import Keys._
+
+object depend {
+  val scalaz = Seq(  "org.scalaz"           %% "scalaz-core"    % "7.0.6"
+                   , "org.scalaz"           %% "scalaz-effect"  % "7.0.6")
+  val scopt  = Seq(  "com.github.scopt"     %% "scopt"          % "3.1.0")
+  val joda   = Seq(  "joda-time"            %  "joda-time"      % "2.1"
+                   , "org.joda"             %  "joda-convert"   % "1.1")
+
+  val specs2 = Seq(  "org.specs2"           %% "specs2-core"         
+                   , "org.specs2"           %% "specs2-scalacheck").map(_ % "2.3.10")
+
+  val parboiled = Seq("org.parboiled"       %% "parboiled"      % "2.0-M2")
+
+  val commonsio = Seq("commons-io"          %  "commons-io"     % "2.4")
+
+  val scrutiny = Seq("com.ambiata"          %% "scrutiny"       % "1.1-20140122014104-1b0d7e9" % "test")
+
+  val rng =      Seq("com.nicta"            %% "rng"            % "1.2.1")
+
+  val kiama =    Seq("com.googlecode.kiama" %% "kiama"          % "1.5.1")
+
+  val resolvers = Seq(
+      Resolver.sonatypeRepo("releases")
+    , Resolver.typesafeRepo("releases")
+    , "artifactory"           at "http://etd-packaging.research.nicta.com.au/artifactory/libs-release-local")
+}
