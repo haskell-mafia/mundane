620ea67ae5125fe1c8b25453fb047934fadda501
markhibberd <mark@hibberd.id.au>
2014-03-23 21:27:54 +1100

    bump saws

--- mundane-daemon/src/main/scala/com/ambiata/mundane/daemon/Daemon.scala
+++ mundane-daemon/src/main/scala/com/ambiata/mundane/daemon/Daemon.scala
@@ -1,4 +1,17 @@
 package com.ambiata.mundane.daemon
 
-trait Daemon {
+trait Daemon[A] {
+  def env: String \/ A
+
+  def run(conf: A): IO[Unit]
+
+  def main(args: Array[String]): Unit =
+    env match {
+      case -\/(field) =>
+        println(s"Missing or invalid field [${field}].")
+        sys.exit(1)
+      case \/-(conf) =>
+        def forever = run(conf).ensuring(forever)
+        forever.unsafePerformIO
+    }
 }
