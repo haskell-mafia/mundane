1c95b6fe17e84ec3b7d2ec0b01c423f39c0d9f97
markhibberd <mark@hibberd.id.au>
2014-04-01 23:53:26 +1100

    Reverted tar back to how it was, to make it inconsistent in a way that doesn't break other projects :/

--- mundane-io/src/main/scala/com/ambiata/mundane/io/Archive.scala
+++ mundane-io/src/main/scala/com/ambiata/mundane/io/Archive.scala
@@ -52,6 +52,7 @@ object Archive {
     !path.isEmpty && (List("sh", "-c", s"tar xfz ${path} -O > /dev/null") ! ProcessLogger(o => (), e => ())) == 0
   }
 
+  // FIX this is not cross platform, arg....
   // FIX clean-up strip-levels this is not a good default API it is overly specific to copper-extract
   def extractTarballStream(tarball: InputStream, dest: FilePath, stripLevels: Int): ResultT[IO, Unit] = ResultT(IO {
     val levels = if (stripLevels > 0) s"-strip-components ${stripLevels}" else ""
@@ -67,8 +68,22 @@ object Archive {
       Result.ok(())
   })
 
+  // FIX testing and cross platform support, shelling out to tar is one thing, throwing random flags around is not ok though
+  def extractTarballStreamTheOtherOneThisIsNotOk(tarball: InputStream, dest: FilePath): ResultT[IO, Unit] = ResultT(IO {
+    val cmd = s"tar xz -C ${dest}"
+    val buffer = new StringBuffer
+    if(!dest.toFile.exists && !dest.toFile.mkdirs)
+      Result.fail(s"Could not extract tarball, extraction dir ${dest} does not exist and could not be created.")
+    else if (!dest.toFile.isDirectory)
+      Result.fail(s"Could not extract tarball, ${dest} is not a directory.")
+    else if ((List("sh", "-c", cmd) #< tarball ! ProcessLogger(o => (), e => buffer.append(s"${e}\n"))) != 0)
+      Result.fail(s"Could not extract tarball, stderr:\n${buffer}")
+    else
+      Result.ok(())
+  })
+
   def extractTarballStreamFlat(tarball: InputStream, dest: FilePath): ResultT[IO, Unit] = for {
-    _ <- extractTarballStream(tarball, dest, 0)
+    _ <- extractTarballStreamTheOtherOneThisIsNotOk(tarball, dest)
     _ <- ResultT(IO {
       val buffer = new StringBuffer
       if ((List("sh", "-c", s"find $dest -type f -exec mv {} ${dest.toFile.getAbsolutePath} \\;") ! ProcessLogger(o => (), e => buffer.append(s"${e}\n"))) != 0)
