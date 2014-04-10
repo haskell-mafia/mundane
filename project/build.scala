3ff673bf02294cd652b2ed30779c2a10e90ce541
markhibberd <mark@hibberd.id.au>
2014-04-10 14:48:13 +1000

    Update promulgate publish to ambiata-oss.

--- project/build.scala
+++ project/build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
-import com.ambiata.promulgate.Plugin._
+
+import com.ambiata.promulgate.project.ProjectPlugin._
 
 object build extends Build {
   type Settings = Def.Setting[_]
@@ -8,7 +9,7 @@ object build extends Build {
   lazy val mundane = Project(
       id = "mundane"
     , base = file(".")
-    , settings = standardSettings ++ Seq(promulgate.pkg := "com.ambiata.mundane")
+    , settings = standardSettings ++ promulgate.library("com.ambiata.mundane", "ambiata-oss")
     , aggregate = Seq(cli, control, data, error, io, parse, reflect, store, testing, time)
     )
     .dependsOn(cli, control, data, error, io, parse, reflect, store, testing, time)
@@ -17,8 +18,6 @@ object build extends Build {
                    projectSettings          ++
                    compilationSettings      ++
                    testingSettings          ++
-                   publishingSettings       ++
-                   promulgate.library       ++
                    Seq(resolvers ++= depend.resolvers)
 
   lazy val projectSettings: Seq[Settings] = Seq(
@@ -31,7 +30,7 @@ object build extends Build {
   lazy val cli = Project(
     id = "cli"
   , base = file("mundane-cli")
-  , settings = standardSettings ++ packageSettings("cli") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("cli") ++ Seq[Settings](
       name := "mundane-cli"
     ) ++ Seq[Settings](libraryDependencies ++= depend.scopt ++ depend.scalaz ++ depend.joda)
   )
@@ -39,7 +38,7 @@ object build extends Build {
   lazy val control = Project(
     id = "control"
   , base = file("mundane-control")
-  , settings = standardSettings ++  packageSettings("control") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("control") ++ Seq[Settings](
       name := "mundane-control"
     ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.specs2)
   )
@@ -48,7 +47,7 @@ object build extends Build {
   lazy val daemon = Project(
     id = "daemon"
   , base = file("mundane-daemon")
-  , settings = standardSettings ++ packageSettings("daemon") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("daemon") ++ Seq[Settings](
       name := "mundane-daemon"
     ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.specs2 ++ depend.scrutiny)
   )
@@ -57,7 +56,7 @@ object build extends Build {
   lazy val data = Project(
     id = "data"
   , base = file("mundane-data")
-  , settings = standardSettings ++ packageSettings("data") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("data") ++ Seq[Settings](
       name := "mundane-data"
     ) ++ Seq[Settings](libraryDependencies ++= depend.rng ++ depend.specs2 ++ depend.kiama)
   )
@@ -65,7 +64,7 @@ object build extends Build {
   lazy val error = Project(
     id = "error"
   , base = file("mundane-error")
-  , settings = standardSettings ++ packageSettings("error") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("error") ++ Seq[Settings](
       name := "mundane-error"
     )
   )
@@ -73,7 +72,7 @@ object build extends Build {
   lazy val io = Project(
     id = "io"
   , base = file("mundane-io")
-  , settings = standardSettings ++ packageSettings("io") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("io") ++ Seq[Settings](
       name := "mundane-io"
     ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.joda ++ depend.specs2 ++ depend.scrutiny)
   )
@@ -82,16 +81,16 @@ object build extends Build {
   lazy val store = Project(
     id = "store"
   , base = file("mundane-store")
-  , settings = standardSettings ++ packageSettings("store") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("store") ++ Seq[Settings](
       name := "mundane-store"
     ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.specs2 ++ depend.bits ++ depend.stream)
   )
-  .dependsOn(control, data, io, testing % "test")
+  .dependsOn(control, data, io, testing % "test", io % "test->test")
 
   lazy val parse = Project(
     id = "parse"
   , base = file("mundane-parse")
-  , settings = standardSettings ++ packageSettings("parse") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("parse") ++ Seq[Settings](
       name := "mundane-parse"
     ) ++ Seq[Settings](libraryDependencies ++= depend.parboiled ++ depend.joda)
   )
@@ -100,7 +99,7 @@ object build extends Build {
   lazy val reflect = Project(
     id = "reflect"
   , base = file("mundane-reflect")
-  , settings = standardSettings ++ packageSettings("reflect") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("reflect") ++ Seq[Settings](
       name := "mundane-reflect"
     )
   )
@@ -108,7 +107,7 @@ object build extends Build {
   lazy val testing = Project(
     id = "testing"
   , base = file("mundane-testing")
-  , settings = standardSettings ++ packageSettings("testing") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("testing") ++ Seq[Settings](
       name := "mundane-testing"
     ) ++ Seq[Settings](libraryDependencies ++= depend.specs2 ++ depend.scrutiny)
   )
@@ -117,7 +116,7 @@ object build extends Build {
   lazy val time = Project(
     id = "time"
   , base = file("mundane-time")
-  , settings = standardSettings ++ packageSettings("time") ++ Seq[Settings](
+  , settings = standardSettings ++ lib("time") ++ Seq[Settings](
       name := "mundane-time"
     ) ++ Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.joda ++ depend.specs2)
   )
@@ -130,26 +129,14 @@ object build extends Build {
     scalacOptions in Test ++= Seq("-Yrangepos")
   )
 
-  def packageSettings(name: String): Seq[Settings] =
-    Seq(promulgate.pkg := s"com.ambiata.mundane.$name")
+  def lib(name: String) =
+    promulgate.library(s"com.ambiata.mundane.$name", "ambiata-oss")
 
   lazy val testingSettings: Seq[Settings] = Seq(
     initialCommands in console := "import org.specs2._",
     logBuffered := false,
     cancelable := true,
     javaOptions += "-Xmx3G"
-  )
-
-  lazy val publishingSettings: Seq[Settings] = Seq(
-    publishMavenStyle := true,
-    publishArtifact in Test := false,
-    pomIncludeRepository := { _ => false },
-    publishTo <<= version { v =>
-      val url = "http://etd-packaging.research.nicta.com.au/artifactory/libs-release-local"
-      val name = "etd-packaging.research.nicta.com.au"
-      Some(Resolver.url(name, new URL(url)))
-    },
-    credentials += Credentials(Path.userHome / ".credentials")
   )
 
   lazy val prompt = shellPrompt in ThisBuild := { state =>
