c00eebe00cfdca4fba4ed95c1615f2f32680c89f
Eric Torreborre <etorreborre@yahoo.com>
2014-06-23 16:25:17 +1000

    added memory units plus conversions

---
+++ mundane-io/src/main/scala/com/ambiata/mundane/io/MemoryConversions.scala
@@ -0,0 +1,67 @@
+package com.ambiata
+package mundane
+package io
+
+/**
+ * Conversion units for memory quantities
+ */
+trait MemoryConversions {
+  implicit class bytesSyntax(value: Long) {
+    def bytes = Bytes(value)
+    def byte  = Bytes(value)
+    def kb    = Kilobytes(value)
+    def kbs   = Kilobytes(value)
+    def mb    = Megabytes(value)
+    def mbs   = Megabytes(value)
+    def gb    = Gigabytes(value)
+    def gbs   = Gigabytes(value)
+    def tb    = Terabytes(value)
+    def tbs   = Terabytes(value)
+  }
+}
+
+sealed trait BytesQuantity {
+  def value: Long
+  def toBytes: Bytes
+  def toKilobytes: Kilobytes
+  def toMegabytes: Megabytes
+  def toGigabytes: Gigabytes
+  def toTerabytes: Terabytes
+}
+case class Bytes(value: Long) extends BytesQuantity {
+  def toBytes    = this
+  def toKilobytes = Kilobytes(value / 1024)
+  def toMegabytes = toKilobytes.toMegabytes
+  def toGigabytes = toKilobytes.toGigabytes
+  def toTerabytes = toKilobytes.toTerabytes
+}
+case class Kilobytes(value: Long) extends BytesQuantity {
+  def toBytes     = Bytes(value * 1024)
+  def toKilobytes = this
+  def toMegabytes = Megabytes(value / 1024)
+  def toGigabytes = toMegabytes.toGigabytes
+  def toTerabytes = toMegabytes.toTerabytes
+}
+case class Megabytes(value: Long) extends BytesQuantity {
+  def toBytes     = toKilobytes.toBytes
+  def toKilobytes = Kilobytes(value * 1024)
+  def toMegabytes = this
+  def toGigabytes = Gigabytes(value / 1024)
+  def toTerabytes = toGigabytes.toTerabytes
+}
+case class Gigabytes(value: Long) extends BytesQuantity {
+  def toBytes     = toMegabytes.toBytes
+  def toKilobytes = toMegabytes.toKilobytes
+  def toMegabytes = Megabytes(value * 1024)
+  def toGigabytes = this
+  def toTerabytes = Terabytes(value / 1024)
+}
+case class Terabytes(value: Long) extends BytesQuantity {
+  def toBytes     = toGigabytes.toBytes
+  def toKilobytes = toGigabytes.toKilobytes
+  def toMegabytes = toGigabytes.toMegabytes
+  def toGigabytes = Gigabytes(value * 1024)
+  def toTerabytes = this
+}
+
+object MemoryConversions extends MemoryConversions
---
+++ mundane-io/src/test/scala/com/ambiata/mundane/io/MemoryConversionsSpec.scala
@@ -0,0 +1,16 @@
+package com.ambiata.mundane.io
+
+import org.specs2.Specification
+
+class MemoryConversionsSpec extends Specification with MemoryConversions { def is = s2"""
+  ${ 1024.bytes.toKilobytes === 1.kb }
+  ${ 1024.kb.toMegabytes    === 1.mb }
+  ${ 1024.mb.toGigabytes    === 1.gb }
+  ${ 1024.gb.toTerabytes    === 1.tb }
+
+  ${ 1.kb.toBytes     === 1024.bytes }
+  ${ 1.mb.toKilobytes === 1024.kbs }
+  ${ 1.gb.toMegabytes === 1024.mbs }
+  ${ 1.tb.toGigabytes === 1024.gbs }
+"""
+}
