72874a01cc369eb7d1de381be3b703d42616ea07
Eric Torreborre <etorreborre@yahoo.com>
2014-09-30 09:22:27 +1000

    keep the original URI in the location

--- mundane-io/src/main/scala/com/ambiata/mundane/io/Location.scala
+++ mundane-io/src/main/scala/com/ambiata/mundane/io/Location.scala
@@ -1,13 +1,20 @@
 package com.ambiata.mundane.io
 
+import java.net.URI
+
 import scalaz._, Scalaz._
 
 sealed trait Location {
   def path: DirPath
+  def uri: URI
 }
-case class HdfsLocation (path: DirPath) extends Location
-case class S3Location   (path: DirPath) extends Location
-case class LocalLocation(path: DirPath) extends Location
+
+case class HdfsLocation (path: DirPath, uri: URI) extends Location
+case class S3Location   (path: DirPath, uri: URI) extends Location {
+  def bucket: String = path.rootname.basename.name
+  def key: String = path.fromRoot.path
+}
+case class LocalLocation(path: DirPath, uri: URI) extends Location
 
 object Location {
   def fromUri(s: String): String \/ Location = try {
@@ -15,14 +22,18 @@ object Location {
     val dirPath = DirPath.unsafe(s)
 
     uri.getScheme match {
-      case "hdfs" => HdfsLocation(dirPath).right
-      case "s3"   => S3Location(dirPath).right
-      case "file" => LocalLocation(dirPath).right
-      case null   => LocalLocation(dirPath).right
+      case "hdfs" => HdfsLocation (dirPath, uri).right
+      case "s3"   => S3Location   (dirPath, uri).right
+      case "file" => LocalLocation(dirPath, uri).right
+      case null   => LocalLocation(dirPath, uri).right
       case _      => s"Unknown or invalid repository scheme [${uri.getScheme}]".left
     }
   } catch {
     case e: java.net.URISyntaxException => e.getMessage.left
   }
+
+  def unapply(location: Location): Option[DirPath] =
+    Some(location.path)
+
 }
 
